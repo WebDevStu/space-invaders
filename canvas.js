63d4c4be40144f82f35a525d437d9dfcf00bb0c0
WebDevStu <git@stewart-anderson.co.uk>
2015-12-02 22:38:34 +0000

    Changes

--- canvas.js
+++ canvas.js
@@ -1,25 +1,31 @@
 
+SI.animations = {};
 
 SI.Canvas = function () {
 
+    this.canvas = document.createElement('canvas');
+    this.ctx = this.canvas.getContext('2d');
 
-    this.create();
+    this.canvas.height = 800;
+    this.canvas.width = 800;
+
+    document.body.appendChild(this.canvas);
+
+
+    SI.animations.draw = this.draw;
+
+    //window.requestAnimationFrame(SI.animations.draw);
 };
 
 
 _.extend(SI.Canvas.prototype, {
 
 
-    create: function () {
 
-        this.canvas = document.createElement('canvas');
-        this.ctx = this.canvas.getContext('2d');
 
-        this.canvas.height = 800;
-        this.canvas.width = 800;
+    draw: function () {
 
-        document.body.appendChild(this.canvas);
+        window.requestAnimationFrame(SI.animations.draw);
     }
-
 
 });
--- game.js
+++ game.js
@@ -4,4 +4,5 @@
 
     var canvas = new SI.Canvas();
 
+
 } ());
